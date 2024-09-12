8fc8c402fd646ff6d1f685ab5fa5d1019837ede3
Gabriel <gabrielmenezes1905@gmail.com>
2024-09-12 00:19:26 -0300

    melhorias

--- src/components/GridContent/mock.js
+++ src/components/GridContent/mock.js
@@ -1,21 +1,15 @@
 export default {
   title: 'O que fazemos',
   html: `
-  <p>O Programa Ponte Digital tem o objetivo de aproximar a academia (IFMA) da sociedade, promovendo o conhecimento e inclusão digital.<p>
+  <p>O Programa Ponte Digital tem o objetivo de aproximar a academia (IFMA) da sociedade, de modo a promover a cidadania e melhores oportunidades ao seu público-alvo.</p>
 
-  <p>
-  Idealizado pela turma de Gerenciamento de Projetos (7º período), o programa nasceu do trabalho em conjunto com as turmas de Fundamentos de Sistemas de Informação (3º período) e Extensão1 (2º período), unindo esforços para atender às necessidades da comunidade.
-  <p>
+  <p>O Programa Ponte Digital surgiu a partir de um desafio feito à turma de Gerenciamento de Projetos, do curso de Sistemas de Informação do IFMA (semestre letivo 2024.1), campus Monte Castelo, no âmbito da curricularização da extensão (PNE 13.005/2014).</p>
 
-  <p>
-  Composto pelos projetos Autonomia Digital, CapacitaTECH e Inspiratech Jovem, o programa visa integrar o conhecimento acadêmico com as demandas reais da sociedade, ampliando as oportunidades de inclusão digital.
-  <p>
+  <p>Assim, a turma de Gerenciamento de Projetos (6º período), Fundamentos de Sistemas de Informação (2º período) e Atividade Curricular de Extensão 1 (3º período), com a mentoria e coordenação geral da profa. Jeane Ferreira (DComp), colaboram na gestão e execução dos três projetos vinculados ao Programa Ponte Digital: Autonomia Digital, CapacitaTECH e Inspiratech Jovem.</p>
 
-  <p>
-  Mais do que uma tarefa acadêmica, essa iniciativa representa a chance de iniciar um ciclo de ações transformadoras, capazes de gerar impacto positivo e duradouro na sociedade.
-  <p>
+  <p>O programa Ponte Digital pretende promover o conhecimento e a inclusão digital, por meio da integração de conhecimentos acadêmicos com as necessidades e experiências da comunidade. Todos os seus integrantes têm a consciência de que, mais do que uma tarefa acadêmica, essa é a oportunidade de iniciar um ciclo de ações que gerem impacto socioeconômico e sejam capazes de transformar a realidade de nossa sociedade para melhor.</p>
 
-  <strong>Isso é o Programa Ponte Digital.<strong>
+  <strong>Isso é o Programa Ponte Digital.</strong>
   `,
   background: false,
 };
